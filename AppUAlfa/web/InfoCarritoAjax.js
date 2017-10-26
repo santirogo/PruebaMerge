1f7a88733768be47e7d09842619d38a232d6313b
Diego Esteban Alvarez Monroy <diesazul96@hotmail.com>
2017-10-26 01:40:12 -0500

    Ubicación del usuario con google API en InfoCarrrito

--- AppUAlfa/web/InfoCarritoAjax.js
+++ AppUAlfa/web/InfoCarritoAjax.js
@@ -1,6 +1,43 @@
 $(document).ready(function(){
     
+    var long;
+    var lati;
     
+//    function initMap() {
+//        var map = new google.maps.Map(document.getElementById('map'), {
+//          center: {lat: 4.714666, lng: -74.072115},
+//          zoom: 19
+//        });
+//        var infoWindow = new google.maps.InfoWindow({map: map});
+//
+//        // Try HTML5 geolocation.
+//        if (navigator.geolocation) {
+//          navigator.geolocation.getCurrentPosition(function(position) {
+//            var pos = {
+//              lat: position.coords.latitude,
+//              lng: position.coords.longitude
+//            };
+//
+//            infoWindow.setPosition(pos);
+//            infoWindow.setContent('Location found.');
+//            map.setCenter(pos);
+//          }, function() {
+//            handleLocationError(true, infoWindow, map.getCenter());
+//          });
+//        } else {
+//          // Browser doesn't support Geolocation
+//          handleLocationError(false, infoWindow, map.getCenter());
+//        }
+//      }
+//
+//      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//        infoWindow.setPosition(pos);
+//        infoWindow.setContent(browserHasGeolocation ?
+//                              'Error: The Geolocation service failed.' :
+//                              'Error: Your browser doesn\'t support geolocation.');
+//      }
+    
+
     var opcion = $('#opcion').val();
         
         $.ajax({
